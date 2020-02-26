444a8d1eb78602361f598ef4475e52a8d0b67368
OlegKriuchenkov <kruchenkovoleh80@gmail.com>
2020-02-26 22:11:27 +0200

    Lab2 development

---
+++ SP_Lab1_1_Kriuchenkov/SP_Lab1_1_Kriuchenkov.cpp
@@ -0,0 +1,151 @@
+﻿#include <stdio.h>
+#include "windows.h"
+#include <locale.h>
+#include <iostream>
+
+using namespace std;
+
+
+
+void PrintError()
+{
+	LPVOID lpvMessage;
+	DWORD dwError = GetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, dwError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)& lpvMessage, 0, NULL);
+	printf("\n%s\n", lpvMessage);
+	LocalFree(lpvMessage);
+	return;
+}
+
+void EncodeToANSIFromUnicode(LPCSTR fileName)
+{
+	HANDLE sourceFile, destinationFile; 
+
+	CHAR buffer[2048];
+	WCHAR encodedCharsToWChar_t[2048];
+
+	DWORD readedChars, writtenChars;  
+	BOOL readFile = true;
+
+	char nameOfDestinationFile[256];
+	snprintf(nameOfDestinationFile, sizeof(nameOfDestinationFile), "%s%s", "UnicodeEncoded", fileName);
+	printf("Результат находится в \n %s\n", nameOfDestinationFile);
+	sourceFile = CreateFileA(fileName, GENERIC_READ, FILE_SHARE_READ,NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (sourceFile == INVALID_HANDLE_VALUE)
+	{
+		PrintError();
+		printf("1 error\n");
+		exit(EXIT_FAILURE);
+	}
+	destinationFile = CreateFileA(nameOfDestinationFile, GENERIC_WRITE, 0,NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (destinationFile == INVALID_HANDLE_VALUE)
+	{
+		PrintError();
+		printf("2 error\n");
+		exit(EXIT_FAILURE);
+	}
+	while (((readFile = ReadFile(sourceFile, &buffer, sizeof(buffer), &readedChars, NULL)) != 0) && (readedChars != 0))
+	{
+		MultiByteToWideChar(GetACP(), 0, buffer, readedChars, encodedCharsToWChar_t, 2048);
+
+		if (!WriteFile(destinationFile, encodedCharsToWChar_t, readedChars * 2, &writtenChars, NULL))
+		{
+			PrintError();
+			printf("3 error\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (readFile == false && readedChars != 0)
+	{
+		PrintError();
+		printf("4 error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	CloseHandle(sourceFile);
+	CloseHandle(destinationFile);
+}
+
+void EncodeToUnicodeFromANSI(LPCSTR fileName)
+{
+	HANDLE sourceFile, destinationFile;
+
+	WCHAR buffer[2048];
+	CHAR WCharsEncodedToCHARS[2048];
+	CCHAR symbols(254);
+	BOOL isSymbolUnknown = false;
+
+	DWORD readedBytes, writtenBytes;  
+	BOOL readFile = true;
+
+	char nameDestinationFile[256];
+	snprintf(nameDestinationFile, sizeof(nameDestinationFile), "%s%s", "ASCIEncoded", fileName);
+	printf("Результат находится в \n %s\n", nameDestinationFile);
+	sourceFile = CreateFileA(fileName, GENERIC_READ, FILE_SHARE_READ,NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (sourceFile == INVALID_HANDLE_VALUE)
+	{
+		PrintError();
+		exit(EXIT_FAILURE);
+	}
+	destinationFile = CreateFileA(nameDestinationFile, GENERIC_WRITE, 0,NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (destinationFile == INVALID_HANDLE_VALUE)
+	{
+		PrintError();
+		exit(EXIT_FAILURE);
+	}
+	if (!ReadFile(sourceFile, &buffer, 2, &readedBytes, NULL))
+	{
+		PrintError();
+		exit(EXIT_FAILURE);
+	}
+	while (((readFile = ReadFile(sourceFile, &buffer, sizeof(buffer), &readedBytes, NULL)) != 0) && (readedBytes != 0))
+	{
+		WideCharToMultiByte(GetACP(), 0, buffer, readedBytes / sizeof(buffer[0]), WCharsEncodedToCHARS,
+			readedBytes / sizeof(buffer[0]), NULL, NULL);
+		if (!WriteFile(destinationFile, WCharsEncodedToCHARS, readedBytes / sizeof(buffer[0]), &writtenBytes, NULL))
+		{
+			PrintError();
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (readFile == false && readedBytes != 0)
+	{
+		PrintError();
+		exit(EXIT_FAILURE);
+	}
+
+	CloseHandle(sourceFile);
+	CloseHandle(destinationFile);
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	if (argc != 3)
+	{
+		fprintf(stderr, "***Ошибка в аргументах для программы!***\n");
+		fprintf(stderr, "-a  - ANSI\n");
+		fprintf(stderr, "-u  - Unicode\n");
+		system("Pause");
+		return 0;
+	}
+	else
+	{
+		printf("%s", argv[1]);
+		if (strcmp(argv[1], "-a") == 0)
+		{
+			EncodeToANSIFromUnicode(argv[2]);
+		}
+		else if (strcmp(argv[1], "-u") == 0)
+		{
+			EncodeToUnicodeFromANSI(argv[2]);
+		}
+		else
+		{
+			fprintf(stderr, "***Ошибка ввода!!!***\n");
+		}
+	}
+	fprintf(stdout, "Успех!");
+	return 0;
+}
+
--- SP_Lab1_Kriuchenkov/SP_Lab1_Kriuchenkov.cpp
+++ SP_Lab1_Kriuchenkov/SP_Lab1_Kriuchenkov.cpp
@@ -1,21 +1,130 @@
-// SP_Lab1_Kriuchenkov.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include "windows.h"
+#include "iostream"
+#include <locale.h>
 
-#include "pch.h"
-#include <iostream>
+using namespace std;
 
-int main()
+void PrintErrorInfo();
+void PrintInfo();
+void NoArguments();
+void PrintMemoryInfo();
+void PrintHardwareInfo();
+bool MemoryError();
+void PrintError();
+
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n"; 
+	setlocale(LC_ALL, "rus");
+
+	if (argc != 2)
+	{
+		NoArguments();
+	}
+	else
+	{
+		if (strcmp(argv[1], "-e") == 0)
+		{
+			PrintErrorInfo();
+		}
+		else if (strcmp(argv[1], "-s") == 0)
+		{
+			PrintHardwareInfo();
+		}
+		else
+		{
+			NoArguments();
+		}
+	}
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void PrintErrorInfo()
+{
+	if (MemoryError())
+	{
+		PrintError();
+	}
+	else
+	{
+		printf("***Memory error!***");
+	}
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void PrintInfo()
+{
+	PrintMemoryInfo();
+	PrintHardwareInfo();
+}
+
+bool MemoryError()
+{
+	DWORD memory = 0;
+	MEMORYSTATUS memoryInfo;
+	GlobalMemoryStatus(&memoryInfo);
+	HLOCAL pAllocMem = LocalAlloc(LMEM_FIXED, memoryInfo.dwTotalVirtual + 5000000000); // total virtual memory + 5 GB
+	if (pAllocMem == NULL)
+	{
+		return true;
+	}
+	else
+	{
+		LocalFree(pAllocMem);
+		return false;
+	}
+}
+
+void PrintError()
+{
+	LPVOID lpvMessage;
+	DWORD error = GetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,NULL, error,MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),(LPWSTR)& lpvMessage, 0, NULL);
+	printf("\n%s\n", lpvMessage);
+	LocalFree(lpvMessage);
+	return;
+}
+
+void NoArguments()
+{
+	printf("No arguments. Please use one of these:\n");
+	printf("'-e' - print error info\n");
+	printf("'-s' - print system information\n");
+	exit(EXIT_FAILURE);
+}
+
+void PrintMemoryInfo()
+{
+	MEMORYSTATUS memoryInfo;
+	GlobalMemoryStatus(&memoryInfo);
+	cout << "Physical and virtual memory" << endl;
+	cout << "Physical memory that is in use now:"
+		<< memoryInfo.dwMemoryLoad << endl;
+	cout << "Physical memory, in bytes:"
+		<< memoryInfo.dwTotalPhys << endl;
+	cout << "Physical memory that is available, in bytes:"
+		<< memoryInfo.dwAvailPhys << endl;
+	cout << "Committed memory limit, in bytes:"
+		<< memoryInfo.dwTotalPageFile << endl;
+	cout << "How much memory the current process can commit, in bytes:"
+		<< memoryInfo.dwAvailPageFile << endl;
+	cout << "Virtual memory, in bytes:"
+		<< memoryInfo.dwTotalVirtual << endl;
+	cout << "Virtual memory that is available, in bytes:"
+		<< memoryInfo.dwAvailVirtual << endl;
+}
+
+void PrintHardwareInfo()
+{
+	SYSTEM_INFO systemInfo;
+	GetSystemInfo(&systemInfo);
+	printf("  Hardware information\n");
+	printf("  Processor architecture: %u\n", systemInfo.wProcessorArchitecture);
+	printf("  Processor type: %u\n", systemInfo.dwProcessorType);
+	printf("  Number of processors: %u\n", systemInfo.dwNumberOfProcessors);
+	printf("  Active processor mask: %u\n", systemInfo.dwActiveProcessorMask);
+	printf("  Page size: %u\n", systemInfo.dwPageSize);
+	printf("  Minimum application address: %p\n", systemInfo.lpMinimumApplicationAddress);
+	printf("  Maximum application address: %p\n", systemInfo.lpMaximumApplicationAddress);
+	printf("  The granularity for the starting address at which virtual memory can be allocated: %u\n", systemInfo.dwAllocationGranularity);
+}
